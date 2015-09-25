0fd9afafe732e329a6765a33b4e36518e2aa686c
Sam Schetterer <samschet@gmail.com>
2015-09-25 00:21:44 -0400

    Adding benchmark version

---
+++ bench.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <time.h>
+
+#include "xorshift.hpp"
+constexpr static size_t num_loop = 3e8;
+constexpr static int num_work = num_loop;
+using namespace std;
+
+template<class seeder>
+void seed_rn(seeder &rn) {
+	rn.seed(0xdeadbeef, 0xcafebabe);
+}
+
+//this forces the compiler to actually
+//do the full rng execution
+//when inside of the inner loop,
+//if this call is inlined,
+//then the compiler can optimize away a bunch of stuff in each case
+//for the first one, some of the reads/writes to memory it generally incurs
+//for the second two, probably can unroll the if statement,
+//and the write to the index
+
+//small loop to remove some cost of function call
+template<class rng>
+void __attribute__ ((noinline))  call_rn (rng &rn) {
+    for (size_t i = 0; i < 3; i++) {
+        rn.rand();
+    }
+}
+
+template<class rng>
+void __attribute__ ((noinline)) work_rn(rng &rn) {
+    rn.do_heavy_work();
+}
+
+template<class rng>
+void grab_many(rng& rn) {
+	for (size_t i = 0; i < num_loop; i++) {
+		call_rn(rn);
+	}
+}
+
+template<class rng>
+void heavy_work(rng& rn) {
+	for (size_t i = 0; i < num_work; i++) {
+		work_rn(rn);
+	}
+}
+
+template<class FN>
+uint64_t time_call(const FN& f, double &total) {
+	clock_t t = clock();
+	f();
+	t = clock() - t;
+	total = t * 1.0/CLOCKS_PER_SEC;
+	return 0;
+}
+
+
+int main() {
+	cout << "timing rngs" << std::endl;
+
+	double time;
+    double lat_time;
+	uint64_t junk_val = 0;
+	junk_val += time_call([]() {
+		xorshift<uint16_t> rn;
+		seed_rn(rn);
+		return grab_many(rn);
+
+	}, time);
+
+	cout << "The plain rng took " << time << " seconds, ";
+    cout << (num_loop * 1.0/time) << " rngs per second " << endl << endl;
+
+    junk_val += time_call([]() {
+        xorshift_bulk<uint64_t, 2> rn;
+        seed_rn(rn);
+        return grab_many(rn);
+
+    }, time);
+
+    cout << "The bulk rng took " << time << " seconds, ";
+    cout << (num_work * 1.0/time) << " rngs per second " << endl;
+
+     junk_val += time_call([]() {
+        xorshift_bulk<uint64_t, 2> rn;
+        seed_rn(rn);
+        heavy_work (rn);
+    }, time);
+
+    cout << "The bulk rng took " << time << " seconds for refilling, ";
+    cout << (num_work * 1.0/time) << " refills per second ";
+    cout << "and " << (1e9 * time / num_work) << "ns per refill" << endl << endl;
+
+    junk_val += time_call([]() {
+        alignas(64) xorshift_sse2<uint64_t, 4> rn;
+        seed_rn(rn);
+        return grab_many(rn);
+
+    }, time);
+
+    cout << "The vector rng took " << time << " seconds, ";
+    cout << (num_loop * 1.0/time) << " rngs per second " << endl;
+
+
+    junk_val += time_call([]() {
+       alignas(64) xorshift_sse2<uint64_t, 4> rn;
+        seed_rn(rn);
+        heavy_work (rn);
+    }, time);
+
+    cout << "The vector rng took " << time << " seconds for refilling, ";
+    cout << (num_work * 1.0/time) << " refills per second ";
+    cout << "and " << (1e9 * time / num_work) << "ns per refill" << endl << endl;
+
+    cout << "Caveat - all these numbers are in a hot loop" << endl;
+}
---
+++ xorshift.hpp
@@ -0,0 +1,154 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <utility>
+
+#include <emmintrin.h>
+
+#define likely(x)    __builtin_expect(!!(x), 1)
+#define unlikely(x)  __builtin_expect(!!(x), 0)
+
+//I suspect this generates some questionable code...
+template<class itype>
+static inline itype do_rng(itype &s0, itype &s1) {
+    s1 ^= s1 << 23;
+    s1 = (s1 ^ s0 ^ (s1 >> 17) ^ (s0 >> 26));
+    return s1 + s0;
+}
+
+template<class int_type>
+class xorshift {
+    uint64_t state[2];
+
+public:
+
+    void seed(uint64_t a, uint64_t b) {
+        state[0] = a;
+        state[1] = b;
+    }
+    //does the work to represent a 'latency hit'
+    //non in this case
+    void do_heavy_work() {}
+    int_type rand() {
+        std::swap(state[0], state[1]);
+        return do_rng(state[0], state[1]);
+    }
+};
+
+//this RNG performs bulk initiation of the
+//array using a more effecient loop
+//but, does do some more memory stores
+//by saving data in an array
+//also is not vectorized
+template<class int_type, size_t size64>
+class xorshift_bulk {
+protected:
+    uint64_t data[size64];
+    uint64_t state[2];
+    uint16_t index;
+
+    static constexpr size_t int_num = size64 * sizeof(uint64_t) / sizeof(int_type);
+
+public:
+    void seed(uint64_t a, uint64_t b) {
+        state[0] = a;
+        state[1] = b;
+        do_heavy_work();
+    }
+    void do_heavy_work() {
+        uint64_t s0 = state[0];
+        uint64_t s1 = state[1];
+        for(size_t i = 0; i < size64; i += 2) {
+            data[i] = do_rng(s1, s0);
+            data[i+1] = do_rng(s0, s1);
+        }
+        state[0] = s0;
+        state[1] = s1;
+        index = 0;
+    }
+
+    int_type rand() {
+        if (unlikely(index == int_num)) {
+            do_heavy_work();
+        }
+        return ((int_type *)data)[index++];
+    }
+};
+#define psllqi(a, b) _mm_slli_epi64(a, b)
+#define psrlqi(a, b) _mm_srli_epi64(a, b)
+
+#define pxor(a, b) _mm_xor_si128(a, b)
+
+#define padd(a, b) _mm_add_epi64(a, b)
+
+
+//allowing gcc to do this by itself
+//with normal operators on the vector type
+//is an absolute disaster...
+
+//this algorithm can trivially be extended to avx2 as well
+//if the machine running even has it
+
+//extern C for easy lookup in object file
+extern "C" {
+
+//avx2 to come once I get that working on windows...
+static __m128i rng_sse2(__m128i &_s0, __m128i &_s1) {
+    //s1 = s1 ^ (s1 << 23)
+    __m128i s1 = _s1;
+    __m128i temp1 = s1;
+    temp1 = psllqi(s1, 23);
+    s1 = pxor(s1, temp1);
+    __m128i s0 = _s0;
+    //s0 >> 26
+    __m128i temp2 = psrlqi(s0, 26);
+    //(s1 >> 17);
+    temp1 = psrlqi(s1, 17);
+    //temp2 ^ temp1 ... duh
+    temp2 = pxor(temp2, temp1);
+    //s1 ^ s0
+    temp1 = pxor(s0, s1);
+    //_s1 = ...
+    s1 = pxor(temp2, temp1);
+    return padd(s1, s0);
+}
+}
+
+template<class int_type, size_t size128>
+class xorshift_sse2 {
+protected:
+    __m128i _data[size128];
+    __m128i state[2];
+    uint16_t index;
+
+    static constexpr size_t int_num = size128 * sizeof(__m128i) / sizeof(int_type);
+
+public:
+    void seed(uint64_t a, uint64_t b) {
+        state[0] = _mm_set_epi64x(a, b);
+        state[1] = _mm_set_epi64x(b, a);
+        do_heavy_work();
+    }
+
+    void do_heavy_work() {
+        __m128i s0 = state[0];
+        __m128i s1 = state[1];
+        __m128i * __restrict data =
+                (__m128i *__restrict ) __builtin_assume_aligned(&_data[0], 16);
+
+
+        for(size_t i = 0; i < size128; i += 2) {
+            data[i] = rng_sse2(s1, s0);
+            data[i+1] = rng_sse2(s0, s1);
+        }
+        state[0] = s0;
+        state[1] = s1;
+        index = 0;
+    }
+
+    int_type rand() {
+        if (unlikely(index == int_num)) {
+            do_heavy_work();
+        }
+        return ((int_type *)_data)[index++];
+    }
+};
